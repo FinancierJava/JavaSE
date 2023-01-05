da298641bcdf38752a71ac5cb758a51884f9acf8
Financier <studyfinancee@gmail.com>
2023-01-05 22:53:06 +0300

    Add DisplayNumbersFrom100To1 to practice cycles package

---
+++ src/practice/cycles/DisplayNumbersFrom100To1.java
@@ -0,0 +1,10 @@
+package practice.cycles;
+
+public class DisplayNumbersFrom100To1 {
+    public static void main(String[] args) {
+
+            for (var i = 100; i >= 1; i--)
+                System.out.println(i);
+
+    }
+}
