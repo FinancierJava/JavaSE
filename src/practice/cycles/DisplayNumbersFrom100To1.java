2a43a0447242f7dcc0af9ede3e7787cee52fba38
Financier <studyfinancee@gmail.com>
2023-01-05 23:02:32 +0300

    Change DisplayNumbersFrom100To1 program

--- src/practice/cycles/DisplayNumbersFrom100To1.java
+++ src/practice/cycles/DisplayNumbersFrom100To1.java
@@ -3,8 +3,14 @@ package practice.cycles;
 public class DisplayNumbersFrom100To1 {
     public static void main(String[] args) {
 
-            for (var i = 100; i >= 1; i--)
-                System.out.println(i);
+        //read source data
+        var from = 100;
+        var to = 1;
+        var step = 1;
+
+        //processing and displaying results
+        for (var i = from; i >= to; i-=step)
+                System.out.print(i + " ");
 
     }
 }
